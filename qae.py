90e6a84a346a1d9c6e8d3d26f7c11222cdaf297d
Thomas Sievert <tcsievert@gmail.com>
2023-03-31 09:57:28 -0700

    ran code

--- qae.py
+++ qae.py
@@ -1,12 +1,16 @@
 # import contextlib
 import os
+
 # import time
 
 import psutil
+
 # print(f'Mem initial - {psutil.Process().memory_info().rss / (1024 * 1024)}')
-import matplotlib.pyplot as plt # big mems
+import matplotlib.pyplot as plt  # big mems
+
 # print(f'Mem pyplot - {psutil.Process().memory_info().rss / (1024 * 1024)}')
-import pennylane as qml # big mems
+import pennylane as qml  # big mems
+
 # print(f'Mem pennylane - {psutil.Process().memory_info().rss / (1024 * 1024)}')
 from pennylane import numpy as np
 from pickle import dump
@@ -16,8 +20,22 @@ from scipy import pi
 # from sklearn.preprocessing import MinMaxScaler
 
 
-def main(ansatz, ansatz_save, params, events, train_size, batch_size, n_ansatz_qubits, 
-            n_latent_qubits, rng_seed, ix, gen, start_time, n_shots, gpu=False):
+def main(
+    ansatz,
+    ansatz_save,
+    params,
+    events,
+    train_size,
+    batch_size,
+    n_ansatz_qubits,
+    n_latent_qubits,
+    rng_seed,
+    ix,
+    gen,
+    start_time,
+    n_shots,
+    gpu=False,
+):
     # os.environ["CUDA_VISIBLE_DEVICES"]=f"{(ix+2)%8}"
     # time.sleep(ix)
     # with contextlib.redirect_stdout(None):
@@ -25,95 +43,116 @@ def main(ansatz, ansatz_save, params, events, train_size, batch_size, n_ansatz_q
     # print(f'Mem setGPU - {psutil.Process().memory_info().rss / (1024 * 1024)}')
     n_trash_qubits = n_ansatz_qubits - n_latent_qubits
     n_wires = n_ansatz_qubits + n_trash_qubits + 1
-    swap_pattern = compute_swap_pattern(n_ansatz_qubits, n_latent_qubits, n_trash_qubits, n_wires)
-    
+    swap_pattern = compute_swap_pattern(
+        n_ansatz_qubits, n_latent_qubits, n_trash_qubits, n_wires
+    )
+
     # qml.about()
 
     # dev = qml.device('qulacs.simulator', wires=n_wires, gpu=gpu, shots=n_shots) # big mems
-    dev = qml.device('default.qubit', wires=n_wires, shots=n_shots)
+    dev = qml.device("default.qubit", wires=n_wires, shots=n_shots)
     # print(f'Mem qml device - {psutil.Process().memory_info().rss / (1024 * 1024)}')
-    qnode = qml.QNode(circuit, dev, diff_method='best')
-    
+    qnode = qml.QNode(circuit, dev, diff_method="best")
+
     # if ix == 0:
     #     print(ansatz_save)
-    print(f'running circuit {ix}')
+    print(f"running circuit {ix}")
     print(ansatz_save)
     config = {
-        'qnode': qnode,
-        'ansatz': ansatz,
-        'ansatz_save': ansatz_save,
-        'params': params,
-        'train_size': train_size,
-        'batch_size': batch_size,
-        'n_latent_qubits': n_latent_qubits,
-        'n_trash_qubits': n_trash_qubits,
-        'n_wires': n_wires,
-        'swap_pattern': swap_pattern,
-        'rng_seed': rng_seed,
-        'ix': ix,
-        'gen': gen,
-        'start_time': start_time,
+        "qnode": qnode,
+        "ansatz": ansatz,
+        "ansatz_save": ansatz_save,
+        "params": params,
+        "train_size": train_size,
+        "batch_size": batch_size,
+        "n_latent_qubits": n_latent_qubits,
+        "n_trash_qubits": n_trash_qubits,
+        "n_wires": n_wires,
+        "swap_pattern": swap_pattern,
+        "rng_seed": rng_seed,
+        "ix": ix,
+        "gen": gen,
+        "start_time": start_time,
     }
-    
+
     best_fid = train(events, config)
     return best_fid
+
 
 def compute_swap_pattern(n_ansatz_qubits, n_latent_qubits, n_trash_qubits, n_wires):
     swap_pattern = []
     for i in range(n_trash_qubits):
-        single_swap = [n_wires-1, 0, 0]
+        single_swap = [n_wires - 1, 0, 0]
         single_swap[1] = n_latent_qubits + i
         single_swap[2] = n_ansatz_qubits + i
 
         swap_pattern.append(single_swap)
-        
+
     return swap_pattern
+
 
 def circuit(params, event=None, config=None):
     # Embed the data into the circuit
-    qml.broadcast(qml.RX, wires=range(config['n_latent_qubits']+config['n_trash_qubits']), pattern='single', parameters=event)
-    qml.Hadamard(wires=config['n_wires']-1)
+    qml.broadcast(
+        qml.RX,
+        wires=range(config["n_latent_qubits"] + config["n_trash_qubits"]),
+        pattern="single",
+        parameters=event,
+    )
+    qml.Hadamard(wires=config["n_wires"] - 1)
 
     # Run the actual circuit ansatz
-    for m in config['ansatz']:
+    for m in config["ansatz"]:
         exec(m)
 
     # Perform the SWAP-Test for a qubit fidelity measurement
-    qml.broadcast(qml.CSWAP, wires=range(config['n_latent_qubits'], config['n_wires']), pattern=config['swap_pattern'])
-    qml.Hadamard(wires=config['n_wires']-1)
+    qml.broadcast(
+        qml.CSWAP,
+        wires=range(config["n_latent_qubits"], config["n_wires"]),
+        pattern=config["swap_pattern"],
+    )
+    qml.Hadamard(wires=config["n_wires"] - 1)
 
     # Puts |1> state as the "state of same fidelities", because expval(PauliZ(|1>)) < 0 => gradient descent
-    qml.PauliX(wires=config['n_wires']-1)
+    qml.PauliX(wires=config["n_wires"] - 1)
 
     # qml.operation.Tensor(qml.PauliZ...) for measuring multiple anscillary wires
-    return qml.expval(qml.PauliZ(wires=config['n_wires']-1))
+    return qml.expval(qml.PauliZ(wires=config["n_wires"] - 1))
+
 
 def train(events, config):
-    if len(config['params']) == 0:
+    if len(config["params"]) == 0:
         return np.array(-2)
-    circuit = config['qnode']
-    rng = np.random.default_rng(seed=config['rng_seed'])
+    circuit = config["qnode"]
+    rng = np.random.default_rng(seed=config["rng_seed"])
     qng_cost = [1]
-    opt = qml.QNGOptimizer(1e-1, approx='block-diag')
+    opt = qml.QNGOptimizer(1e-1, approx="block-diag")
 
     # Initialized to 2 b/c worst performance would be value of 1.
     best_perf = [2, np.array([])]
-    stop_check = [[0,0], [0,0]]
+    stop_check = [[0, 0], [0, 0]]
     stop_check_factor = 40
     step_size_factor = 0
-    theta = pi * rng.random(size=np.shape(config['params']), requires_grad=True)
+    theta = pi * rng.random(size=np.shape(config["params"]), requires_grad=True)
     step = 0
 
     while True:
-        events_batch = rng.choice(events, config['batch_size'], replace=False)
+        events_batch = rng.choice(events, config["batch_size"], replace=False)
 
         grads = np.zeros((events_batch.shape[0], theta.shape[0]))
         costs = np.zeros(events_batch.shape[0])
 
         # iterating over all the training data
         for i in range(events_batch.shape[0]):
-            fub_stud = qml.metric_tensor(config['qnode'], approx="block-diag")(theta, event=events_batch[i], config=config)
-            grads[i] = np.matmul(fub_stud, opt.compute_grad(config['qnode'], (theta, events_batch[i], config), {})[0][0])
+            fub_stud = qml.metric_tensor(config["qnode"], approx="block-diag")(
+                theta, event=events_batch[i], config=config
+            )
+            grads[i] = np.matmul(
+                fub_stud,
+                opt.compute_grad(config["qnode"], (theta, events_batch[i], config), {})[
+                    0
+                ][0],
+            )
             costs[i] = circuit(theta, event=events_batch[i], config=config)
 
         if best_perf[0] > costs.mean(axis=0):
@@ -128,61 +167,88 @@ def train(events, config):
             stop_check[0][1] = np.std(qng_cost[-40:-20], axis=0)
             stop_check[1][0] = np.mean(qng_cost[-20:], axis=0)
             stop_check[1][1] = np.std(qng_cost[-20:], axis=0)
-            if np.isclose(stop_check[0][0], stop_check[1][0], atol=np.amin([stop_check[0][1], stop_check[1][1]])):
+            if np.isclose(
+                stop_check[0][0],
+                stop_check[1][0],
+                atol=np.amin([stop_check[0][1], stop_check[1][1]]),
+            ):
                 step_size_factor -= 1
                 stop_check_factor = step + 20
                 if step_size_factor < -5:
                     break
-                    
+
         step += 1
-    
+
     # big mems
     # print(f'Mem done training - {psutil.Process().memory_info().rss / (1024 * 1024)}')
     script_path = os.path.dirname(os.path.realpath(__file__))
-    destdir = os.path.join(script_path, 'qae_runs_%s' % config['start_time'])
+    destdir = os.path.join(script_path, "qae_runs_%s" % config["start_time"])
     if not os.path.exists(destdir):
         os.makedirs(destdir)
-    
-    destdir_thetas = os.path.join(destdir, 'opt_thetas')
+
+    destdir_thetas = os.path.join(destdir, "opt_thetas")
     if not os.path.exists(destdir_thetas):
         os.makedirs(destdir_thetas)
-    filepath_thetas = os.path.join(destdir_thetas, '%02d_%03dga_best%.e_data_theta' % (config['ix'], config['gen'], config['train_size']))
+    filepath_thetas = os.path.join(
+        destdir_thetas,
+        "%02d_%03dga_best%.e_data_theta"
+        % (config["ix"], config["gen"], config["train_size"]),
+    )
     np.save(filepath_thetas, best_perf[1])
-    
-    destdir_ansatz = os.path.join(destdir, 'opt_ansatz')
+
+    destdir_ansatz = os.path.join(destdir, "opt_ansatz")
     if not os.path.exists(destdir_ansatz):
         os.makedirs(destdir_ansatz)
     # Make ansatz represented in list-of-dicts-of-qubits
-    filepath_ansatz = os.path.join(destdir_ansatz, '%02d_%03dga_best%.e_data_ansatz' % (config['ix'], config['gen'], config['train_size']))
+    filepath_ansatz = os.path.join(
+        destdir_ansatz,
+        "%02d_%03dga_best%.e_data_ansatz"
+        % (config["ix"], config["gen"], config["train_size"]),
+    )
     with open(filepath_ansatz, "wb") as f:
-        dump(config['ansatz_save'], f)
+        dump(config["ansatz_save"], f)
     # Make ansatz to run using loop
-    filepath_run = os.path.join(destdir_ansatz, '%02d_%03dga_best%.e_run_ansatz' % (config['ix'], config['gen'], config['train_size']))
-    np.save(filepath_run, config['ansatz'])
+    filepath_run = os.path.join(
+        destdir_ansatz,
+        "%02d_%03dga_best%.e_run_ansatz"
+        % (config["ix"], config["gen"], config["train_size"]),
+    )
+    np.save(filepath_run, config["ansatz"])
     # Make ansatz to draw in output files
-    filepath_draw = os.path.join(destdir_ansatz, '%02d_%03dga_best%.e_draw_ansatz' % (config['ix'], config['gen'], config['train_size']))
-    ansatz_draw = qml.draw(config['qnode'], decimals=None, expansion_strategy='device')(theta, event=events_batch[0], config=config)
+    filepath_draw = os.path.join(
+        destdir_ansatz,
+        "%02d_%03dga_best%.e_draw_ansatz"
+        % (config["ix"], config["gen"], config["train_size"]),
+    )
+    ansatz_draw = qml.draw(config["qnode"], decimals=None, expansion_strategy="device")(
+        theta, event=events_batch[0], config=config
+    )
     with open(filepath_draw, "w") as f:
         f.write(ansatz_draw)
-        
-    destdir_curves = os.path.join(destdir, 'qml_curves')
+
+    destdir_curves = os.path.join(destdir, "qml_curves")
     if not os.path.exists(destdir_curves):
         os.makedirs(destdir_curves)
-    filepath_curves = os.path.join(destdir_curves, "%02d_%03dga_QNG_Descent-%d_data.pdf" % (config['ix'], config['gen'], config['train_size']))
-    plt.figure(config['train_size'])
+    filepath_curves = os.path.join(
+        destdir_curves,
+        "%02d_%03dga_QNG_Descent-%d_data.pdf"
+        % (config["ix"], config["gen"], config["train_size"]),
+    )
+    plt.figure(config["train_size"])
     plt.style.use("seaborn")
-    plt.plot(qng_cost, "g", label="QNG Descent - %d data" % config['train_size'])
+    plt.plot(qng_cost, "g", label="QNG Descent - %d data" % config["train_size"])
     plt.ylabel("Loss (-1 * Fid.)")
     plt.xlabel("Optimization steps")
     plt.legend()
     plt.savefig(filepath_curves)
-    
+
     # big mems
     # print(f'Mem saved files - {psutil.Process().memory_info().rss / (1024 * 1024)}')
-    if config['ix'] == 0:
-        print(f'Mem qml final - {psutil.Process().memory_info().rss / (1024 * 1024)}')
-        
-    return -1 * best_perf[0] # Returning Fid estimate
+    if config["ix"] == 0:
+        print(f"Mem qml final - {psutil.Process().memory_info().rss / (1024 * 1024)}")
+
+    return -1 * best_perf[0]  # Returning Fid estimate
+
 
 # def compute_auroc(filepath_thetas, config):
 #     f_ix = filepath_thetas.find('qae_runs')
@@ -192,7 +258,7 @@ def train(events, config):
 #     # print(opt_theta)
 #     # print(type(opt_theta))
 #     # print(type(opt_theta[0]))
-    
+
 #     events_bb1 = np.load('10k_dijet_bb1.npy', requires_grad=False)
 #     # print(events_bb1)
 #     # print(type(events_bb1))
@@ -203,11 +269,11 @@ def train(events, config):
 #     # print(classes)
 #     scaler = MinMaxScaler(feature_range=(0, pi))
 #     events_bb1 = scaler.fit_transform(events_bb1)
-    
+
 #     f = open('events_LHCO2020_BlackBox1.masterkey', 'r')
 #     event_classes = np.genfromtxt(f, delimiter=',')
 #     event_class = event_classes[classes.tolist()]
-    
+
 #     cost = []
 #     for i in range(np.size(events_bb1, axis=0)):
 #         # if i == 0:
@@ -223,7 +289,7 @@ def train(events, config):
 #     auroc = roc_auc_score(event_class, cost)
 #     fpr, tpr, thresholds = roc_curve(event_class, cost)
 #     bkg_rejec = 1 - fpr
-    
+
 #     filepath_curves = os.path.join(destdir_curves, "%02d_%03dga_roc_bb1-%d_data.pdf" % (config['ix'], config['gen'], config['train_size']))
 #     plt.figure(config['train_size'])
 #     plt.style.use("seaborn")
@@ -233,5 +299,5 @@ def train(events, config):
 #     plt.ylabel('Sig.  Acceptance')
 #     plt.legend()
 #     plt.savefig(filepath_curves)
-    
+
 #     return auroc
