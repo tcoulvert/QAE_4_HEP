cd915648670ecaa7e0cb0ce5272aff9eb6ac3779
Thomas Sievert <tcsievert@gmail.com>
2023-10-10 13:32:50 -0700

    test and debug GA

--- GA_QAE_Runner.py
+++ GA_QAE_Runner.py
@@ -51,7 +51,7 @@ def main(rng_seed):
         "n_wires": 6,  # allows us to use GA to optimize subsets of a circuit
         "n_trash_qubits": 2,
         "n_latent_qubits": 1,
-        "n_shots": 1000,  # ~1000
+        "n_shots": 100,  # ~1000
         "events": events,
         "batch_size": 8,  # powers of 2, between 1 to 32
         "GPU": False,
@@ -63,8 +63,9 @@ def main(rng_seed):
     ga_output_path = os.path.dirname(os.path.realpath(__file__))
 
     config = gav.Config(qae_main, vqc_config, genepool, ga_output_path)
-    config.init_pop_size = 1000
-    config.pop_size = 20
+    config.init_pop_size = 100
+    config.pop_size = 10
+    config.max_moments = 4
 
     ga = gav.setup(config)
     ga.evolve()
